f2b7e57108b04be002389523e5c64a110d148c81
ABHI <ABHI@LAPTOP-399HN2CI>
2020-02-23 12:49:11 +0530

    9652

---
+++ src/main/java/in/nit/module/Employee.java
@@ -0,0 +1,16 @@
+package in.nit.module;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(generator = "increment")
+private Integer eid;
+private String ename;
+}
